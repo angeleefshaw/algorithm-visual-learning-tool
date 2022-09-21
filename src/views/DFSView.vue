ddc516977220b438d79c8482146b7ac761542d7c
angeleeshaw <59425777+angeleefshaw@users.noreply.github.com>
2022-09-20 21:35:23 -0400

    Reactivity in DST could be improved by using vues reactive method

--- src/views/DFSView.vue
+++ src/views/DFSView.vue
@@ -1,41 +1,41 @@
 <script setup>
-  import { onMounted, ref } from 'vue';
+  import { reactive, ref } from 'vue';
   import Node from '../modules/DFS/composables/CreateNode.js'
   import TreeNode from '../components/TreeNode.vue'
-import { root } from 'postcss';
 
   //DATA
-  let inputVal = ref('');
-  let selectedNode = ref('') ;
-  let vertexActive = ref(false);
-  let rootNode = ref({})
-  let selectError = ref(false);
-
-  const tree = []
+  let data = reactive({
+    "inputVal": '',
+    "selectedNode": '',
+    "vertexActive": false,
+    "rootNode": '',
+    "selectError": false,
+    "tree": []
+  })
 
   //ON CREATE
-  rootNode = new Node("A")
-  rootNode.addChild("B")
-  const Node2 = rootNode.children[0]
+  data.rootNode = new Node("A")
+  data.rootNode.addChild("B")
+  const Node2 = data.rootNode.children[0]
   Node2.addChild("C")
   Node2.addChild("D")
-  const Node3 = rootNode.children[2]
+  const Node3 = data.rootNode.children[2]
 
-  tree.push(rootNode)
+  data.tree.push(data.rootNode)
 
   //METHODS
   function addNode(nv) {
-    if (!selectedNode) {
-      this.selectError = true;
+    if (this.data.selectedNode.length < 1) {
+      this.data.selectError = ref(true);
       return;
     }
-    console.log(this.selectedNode)
-    console.log(this.inputVal)
+    console.log(this.data.selectedNode)
+    console.log(this.data.inputVal)
   }
 
   function setVertex (click) {
-    this.vertexActive = ref(true);
-    this.selectedNode = ref(click.target.textContent)
+    this.data.vertexActive = ref(true);
+    this.data.selectedNode = ref(click.target.textContent)
   }
 
 </script>
@@ -43,20 +43,16 @@ import { root } from 'postcss';
 
 <template>
   <div class="about justify-center flex-col col-span-2">
-
+    <span v-if="data.selectError" class="text-xs text-red-600 my-1">
+        Please select a vertex to create a node
+    </span>
     <div class="pb-10">
       <span class="text-base">Add Node</span>
       <input 
-        v-model="inputVal"
+        v-model="data.inputVal"
         class="border-2 mx-4 px-3 py-1" 
         type="text"
       >
-      <span 
-        v-if="selectError" 
-        class="text-xs text-neutral-600" 
-      >
-        Please select a vertex to create a node
-      </span>
       <button 
         type="button" 
         class="border-2 rounded px-3 py-1 hover:bg-blue-200"
@@ -67,7 +63,7 @@ import { root } from 'postcss';
 
     <div @click="setVertex($event)">
       <TreeNode 
-        v-for="node in tree" 
+        v-for="node in data.tree" 
         :key="node.name" 
         :node="node.children" 
         :name="node.name">
